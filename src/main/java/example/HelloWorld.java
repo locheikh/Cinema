eda99b6369b338f14f4d0cf1b40517ab9ef12ef4
loche <lo.cheikh92110@gmail.com>
2017-07-22 20:54:24 +0200

    Initial commit

---
+++ src/main/java/example/HelloWorld.java
@@ -0,0 +1,26 @@
+package example;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorld extends ActionSupport  {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final String MESSAGE = "Struts is up and running ...";
+
+	public String execute() throws Exception {
+		setMessage(MESSAGE);
+		return SUCCESS;
+	}
+
+	private String message;
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
